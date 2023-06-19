297b52acf1c2aea824ecab31968b02bfe534e46a
Felipe Pelletier <felipelletier@gmail.com>
2023-06-19 15:41:23 -0300

    cree funcion con variables y ciclo adentro

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
-let cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos: "));
-
 function ingresarAlumnos() {
-  return cantidadAlumnos;
-}
+  let cantidadAlumnos = parseInt(prompt("Ingrese la cantidad de alumnos: "));
 
-for (let i = 0; i < cantidadAlumnos; i++) {
-  let nombreAlumno = prompt("Ingrese el nombre del alumno " + (i + 1) + ":");
-  let nota1 = parseFloat(
-    prompt("Ingrese la primer nota del alumno " + (i + 1) + ":")
-  );
-  let nota2 = parseFloat(
-    prompt("Ingrese la segunda nota del alumno " + (i + 1) + ":")
-  );
-
-  let promedioNotas = (nota1 + nota2) / 2;
-
-  if (promedioNotas >= 5) {
-    alert(nombreAlumno + " esta aprobado con un promedio de " + promedioNotas);
-  } else {
-    alert(
-      nombreAlumno + " esta desaprobado con un promedio de " + promedioNotas
+  for (let i = 0; i < cantidadAlumnos; i++) {
+    let nombreAlumno = prompt("Ingrese el nombre del alumno " + (i + 1) + ":");
+    let nota1 = parseFloat(
+      prompt("Ingrese la primer nota del alumno " + (i + 1) + ":")
     );
+    let nota2 = parseFloat(
+      prompt("Ingrese la segunda nota del alumno " + (i + 1) + ":")
+    );
+
+    let promedioNotas = (nota1 + nota2) / 2;
+
+    if (promedioNotas >= 5) {
+      alert(
+        nombreAlumno + " esta aprobado con un promedio de " + promedioNotas
+      );
+    } else {
+      alert(
+        nombreAlumno + " esta desaprobado con un promedio de " + promedioNotas
+      );
+    }
   }
 }
+
+ingresarAlumnos();
